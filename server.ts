f1df0c4fe538342b5559b4e310bfeb5793acae69
Tristan Fregona <fregona.tristan.pro@gmail.com>
2023-09-11 10:15:04 +0200

    testing no env

--- server.ts
+++ server.ts
@@ -1,3 +1,3 @@
 import app from './app.js'
 
-export default app.listen(process.env.PORT,() => {console.log(`Listenning to ${process.env.PORT} for ${process.env.ENV}`)})
+export default app.listen(process.env.PORT || 3100,() => {console.log(`Listenning to ${process.env.PORT || 3100} for ${process.env.ENV}`)})
